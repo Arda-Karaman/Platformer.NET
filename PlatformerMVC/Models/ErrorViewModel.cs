28ac64ed8c3722fe264d489334b30b4185f9af84
Arda-Karaman <arda.karaman@ug.bilkent.edu.tr>
2024-02-20 20:02:51 +0300

    Add project files.

---
+++ PlatformerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PlatformerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
